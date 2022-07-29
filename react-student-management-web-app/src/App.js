768a056ce5d07fb66664907a6eafbd3095dc4144
NnQuang20 <quangn@hcl.com>
2022-07-29 07:09:58 +0000

    update

--- react-student-management-web-app/src/App.js
+++ react-student-management-web-app/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <a href="/students" className="navbar-brand">
-            Vietnam_QuangNguyen1
+            Vietnam_QuangNguyen
           </a>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
